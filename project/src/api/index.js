41be52a00c8f6272bc0a24962b348d6949a89313
xyx <1584961631@qq.com>
2020-02-27 10:53:53 +0800

    api修改

--- project/src/api/index.js
+++ project/src/api/index.js
@@ -1,21 +1,34 @@
 import axios from "../utils/http"
 import base from "./base"
+
 const api = {
-    // 首页主要内容接口
-    getHomeData(){
-        return axios.get(base.baseUrl + base.homeData);
+    //获取身体症状数据接口
+    getPhysicalSymptoms(){
+        return axios.get(base.baseUrl + base.physicalSymptoms);
     },
-    // 测试的题目接口
-    getCeshiData(){
-        return axios.get(base.baseUrl + base.ceshiData);
+    //获取今日目标数据接口
+    getTarget(){
+        return axios.get(base.baseUrl + base.target);
     },
-    // 测试结果接口
-    getCeshiResultData(){
-        return axios.get(base.baseUrl + base.ceshiResultData);
+    //身体症状添加数据接口
+    getAddPhysicalSymptoms(params){
+        return axios.get(base.baseUrl + base.addPhysicalSymptoms,{
+            params
+        });
     },
-    // 每日食谱接口
-    getMeirishipuData(){
-        return axios.get(base.baseUrl + base.meirishipuData);
+    //今日目标添加数据接口
+    getAddTarget(params){
+        return axios.get(base.baseUrl + base.addTarget,{
+            params
+        });
     },
+    //症状调理数据接口
+    getZhengzhuang(params){
+        return axios.get(base.baseUrl + base.zhengzhuang,{
+            params
+        });
+    }
+
 }
-export default api
+
+export default api;
