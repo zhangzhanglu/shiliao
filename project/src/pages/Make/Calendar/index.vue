0fbcfe52a88c7e1c2e2773cfc9a5669a5227f060
xyx <1584961631@qq.com>
2020-02-27 11:10:01 +0800

    私人定制

---
+++ project/src/pages/Make/Calendar/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <Calendar
+      v-on:choseDay="clickDay"
+      v-on:changeMonth="changeDate"
+      v-on:isToday="clickToday"
+     :sundayStart="true" 
+    ></Calendar>
+  </div>
+</template>
+
+<script>
+import Calendar from "vue-calendar-component";
+export default {
+    components:{
+        Calendar
+    },
+  methods: {
+    clickDay(data) {
+      console.log(data); //选中某天
+    },
+    changeDate(data) {
+      console.log(data); //左右点击切换月份
+    },
+    clickToday(data) {
+      console.log(data); //跳到了本月
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
